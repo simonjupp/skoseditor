06378ef9d5e3119f416bd1270ed5fc4001e333c9
simon.jupp <simon.jupp@c0d8d085-843f-0410-93d5-51194930ba31>
2008-11-20 15:42:29 +0000

    - Fixed bug where new related concepts were not being typed as skos:Concept - Added context dependant dialogs on some of the button to add object relations

---
+++ trunk/org.sealife.skos/src/org/sealife/skos/editor/panels/SKOSConceptSchemeSelectorPanel.java
@@ -0,0 +1,203 @@
+package org.sealife.skos.editor.panels;
+
+import org.protege.editor.core.ui.view.ViewComponent;
+import org.protege.editor.core.ui.view.ViewComponentPlugin;
+import org.protege.editor.core.ui.view.ViewComponentPluginAdapter;
+import org.protege.editor.core.ui.workspace.Workspace;
+import org.protege.editor.owl.OWLEditorKit;
+import org.protege.editor.owl.ui.UIHelper;
+import org.protege.editor.owl.ui.renderer.OWLSystemColors;
+import org.protege.editor.owl.ui.selector.AbstractSelectorPanel;
+import org.sealife.skos.editor.views.SKOSConceptSchemeListViewComponent;
+import org.semanticweb.owl.model.OWLIndividual;
+import org.semanticweb.owl.model.OWLOntology;
+
+import javax.swing.*;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+import java.util.Set;
+/*
+ * Copyright (C) 2007, University of Manchester
+ *
+ * Modifications to the initial code base are copyright of their
+ * respective authors, or their employers as appropriate.  Authorship
+ * of the modifications may be determined from the ChangeLog placed at
+ * the end of this file.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/**
+ * Author: Matthew Horridge, Simon Jupp<br>
+ * The University Of Manchester<br>
+ * Medical Informatics Group<br>
+ * Date: 05-Jul-2006<br>
+ * <br>
+ * <p/> matthew.horridge@cs.man.ac.uk<br>
+ * www.cs.man.ac.uk/~horridgm<br>
+ * <br>
+ */
+public class SKOSConceptSchemeSelectorPanel extends AbstractSelectorPanel<OWLIndividual> {
+
+    private SKOSConceptSchemeListViewComponent viewComponent;
+
+    private Set<OWLOntology> ontologies;
+
+    public SKOSConceptSchemeSelectorPanel(OWLEditorKit eKit) {
+        this(eKit, true);
+    }
+
+    public SKOSConceptSchemeSelectorPanel(OWLEditorKit eKit, boolean editable) {
+        this(eKit, editable, eKit.getModelManager().getActiveOntologies());
+    }
+
+    public SKOSConceptSchemeSelectorPanel(OWLEditorKit eKit, boolean editable, Set<OWLOntology> ontologies) {
+        this(eKit, editable, ontologies, ListSelectionModel.SINGLE_SELECTION);
+    }
+
+    public SKOSConceptSchemeSelectorPanel(OWLEditorKit eKit, int selectionMode) {
+        this(eKit, true, null, selectionMode);
+    }
+
+    /**
+     * Builds an OWLIndividualSelectorPanel with the input selection mode. The
+     * valid values are the same described in the constants in
+     * javax.swing.ListSelectionModel (the default is
+     * ListSelectionModel.SINGLE_SELECTION)
+     *
+     * @param eKit
+     * @param selectionMode
+     */
+    public SKOSConceptSchemeSelectorPanel(OWLEditorKit eKit, boolean editable, Set<OWLOntology> ontologies, int selectionMode) {
+        super(eKit, editable, false);
+        this.ontologies = ontologies;
+        createUI();
+        this.viewComponent.setSelectionMode(selectionMode);
+    }
+
+    public static OWLIndividual showDialog(OWLEditorKit owlEditorKit) {
+        SKOSConceptSchemeSelectorPanel panel = new SKOSConceptSchemeSelectorPanel(owlEditorKit, true, owlEditorKit.getModelManager().getOntologies(), ListSelectionModel.SINGLE_SELECTION);
+
+        int ret = new UIHelper(owlEditorKit).showDialog("Create a new SKOS Concept Scheme", panel, panel.viewComponent);
+
+        if (ret == JOptionPane.OK_OPTION) {
+            OWLIndividual ind = panel.getSelectedObject();
+            panel.viewComponent.dispose();
+            return ind;
+        }
+        else {
+            panel.viewComponent.dispose();
+            return null;
+        }
+    }
+
+    public void setSelection(OWLIndividual ind) {
+        if (viewComponent.getView() != null) {
+            viewComponent.getView().setPinned(false);
+        }
+        viewComponent.setSelectedIndividual(ind);
+    }
+
+
+    public void setSelection(Set<OWLIndividual> entities) {
+        viewComponent.setSelectedIndividuals(entities);
+    }
+
+
+    public OWLIndividual getSelectedObject() {
+        return viewComponent.getSelectedIndividual();
+    }
+
+    public Set<OWLIndividual> getSelectedObjects() {
+        return viewComponent.getSelectedIndividuals();
+    }
+
+
+    public void dispose() {
+        viewComponent.dispose();
+    }
+
+    protected ViewComponentPlugin getViewComponentPlugin() {
+        return new ViewComponentPluginAdapter() {
+            public String getLabel() {
+                return "Individuals";
+            }
+
+            public Workspace getWorkspace() {
+                return getOWLEditorKit().getWorkspace();
+            }
+
+            public ViewComponent newInstance() throws ClassNotFoundException,
+                    IllegalAccessException, InstantiationException {
+                viewComponent = new SKOSConceptSchemeListViewComponent(){
+                    protected void setupActions() {
+                        if (isEditable()){
+                            super.setupActions();
+                        }
+                    }
+
+                    protected Set<OWLOntology> getOntologies() {
+                        if (ontologies != null){
+                            return ontologies;
+                        }
+                        return super.getOntologies();
+                    }
+                };
+                viewComponent.setup(this);
+                return viewComponent;
+            }
+
+            public Color getBackgroundColor() {
+                return OWLSystemColors.getOWLIndividualColor();
+            }
+        };
+    }
+
+    public void setOntologies(Set<OWLOntology> ontologies) {
+
+    }
+
+    public void addSelectionListener(ChangeListener listener) {
+        viewComponent.addChangeListener(listener);
+    }
+
+    public void removeSelectionListener(ChangeListener listener) {
+        viewComponent.removeChangeListener(listener);
+    }
+
+    /**
+     * @deprecated Use <code>getSelectedObject</code>
+     * @return
+     */
+    public OWLIndividual getSelectedIndividual() {
+        return getSelectedObject();
+    }
+
+    /**
+     * @deprecated Use <code>getSelectedObjects</code>
+     * @return
+     */
+    public Set<OWLIndividual> getSelectedIndividuals() {
+        return getSelectedObjects();
+    }
+
+    /**
+     * @deprecated Use <code>setSelection</code>
+     * @param ind
+     */
+    public void setSelectedIndividual(OWLIndividual ind) {
+        setSelection(ind);
+    }
+}
